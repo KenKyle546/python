e4d970c0085d2535ad020827b9e018eaaba365d9
Ken Kyle <104080895+KenKyle546@users.noreply.github.com>
2022-04-27 13:21:46 -0600

    Add files via upload

---
+++ python/Air_Hockey/air_hockey/game/casting/rectangle.py
@@ -0,0 +1,28 @@
+from game.casting.point import Point
+"""
+    Do you want to modify the field? if so, you are in the right place!
+"""
+
+class Rectangle:
+    """A 4-sided flat shape with straight sides."""
+
+    def __init__(self, position, size):
+        """Constructs a new Rectangle."""
+        self._position = Point()
+        self._size = Point() 
+
+    def get_position(self):
+        """Gets the top left point of the rectangle.
+        
+        Returns:
+            An instance of Point containing the top left coordinates.
+        """
+        return self._position
+
+    def get_size(self):
+        """Gets the size of the rectangle.
+        
+        Returns:
+            An instance of Point containing the width and height.
+        """
+        return self._size
